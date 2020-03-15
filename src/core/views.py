011c0076215f5b66fed21071604093f7c88b0759
Paul Hoekman <paul@penguinprotocols.com>
2020-03-15 18:13:55 +0200

    Views fixing 2

--- src/core/views.py
+++ src/core/views.py
@@ -413,7 +413,7 @@ def load_baseline(request):
         { "id": 11, "title": "UM Community", "parent": None, "slug": "/community/", "position": 2 },
         { "id": 12, "title": "People", "parent": 11, "slug": "/community/people/", "position": 1, "content": "<p>This page contains an overview of people who are or have been active in the urban metabolism community.</p>" },
         { "id": 13, "title": "Organisations", "parent": 11, "slug": "/community/organisations/", "position": 2 },
-        { "id": 14, "title": "Our Projects", "parent": 11, "slug": "/community/projects/", "position": 3 },
+        { "id": 14, "title": "Projects", "parent": 11, "slug": "/community/projects/", "position": 3 },
         { "id": 15, "title": "News", "parent": 11, "slug": "/community/news/", "position": 4 },
         { "id": 16, "title": "Events", "parent": 11, "slug": "/community/events/", "position": 5 },
         { "id": 17, "title": "Forum", "parent": 11, "slug": "/community/forum/", "position": 6 },
