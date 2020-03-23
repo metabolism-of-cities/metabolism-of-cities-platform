8d97cd00d469fcdc539bf0984a8286a9c3d29712
​christian <christian@penguinprotocols.com>
2020-03-23 17:35:39 +0200

    redirected to site after save from admin

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from .models import *
+from django.shortcuts import redirect
 # Register your models here.
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'site', 'parent', 'active']
     search_fields = ['title', 'site']
+    def response_change(self, request, obj):
+        print(obj.record.description)
+        url = obj.get_absolute_url()
+        return redirect(url)
+
 
 admin.site.register(Record)
 admin.site.register(Event)
