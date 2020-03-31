87947ce8167b64d6d2588f48152fd49732b3267b
Fernando Larios <fernando@penguinprotocols.com>
2020-03-30 21:15:20 -0600

    show customs fields in the articles design

--- src/core/static/js/scripts.js
+++ src/core/static/js/scripts.js
@@ -1,3 +1,26 @@
-$(document).ready(function() {
-    $('.field-article').addClass("hide");
+(function ($) {
+$(document).ready(function(){
+    $(".field-article").addClass("hide");
+    $(".field-header_title").addClass("hide");
+    $(".field-header_subtitle").addClass("hide");
+    $(".field-header_image").addClass("hide");
+
+    $(".field-header #id_header").change(function () {
+        var value = $(this).val();
+
+        if (value == 'full') {
+            $(".field-header_title").show("slow").removeClass("hide");
+            $(".field-header_subtitle").show("slow").removeClass("hide");
+        } else {
+            $(".field-header_title").hide("slow").addClass("hide");
+            $(".field-header_subtitle").hide("slow").addClass("hide");
+        }
+
+        if (value == 'image') {
+            $(".field-header_image").show("slow").removeClass("hide");
+        } else {
+            $(".field-header_image").hide("slow").addClass("hide");
+        }
+    }).change();
 });
+})(jQuery);
