217dbd1fa427cfb01404754bbe274a005c8317c4
Paul Hoekman <paul@penguinprotocols.com>
2020-04-07 14:55:36 +0200

    Coding standards 5

--- src/core/static/js/scripts.js
+++ src/core/static/js/scripts.js
@@ -1,26 +1,26 @@
 (function ($) {
 $(document).ready(function(){
-    $(".field-article").addClass("hide");
-    $(".field-header_title").addClass("hide");
-    $(".field-header_subtitle").addClass("hide");
-    $(".field-header_image").addClass("hide");
+  $(".field-article").addClass("hide");
+  $(".field-header_title").addClass("hide");
+  $(".field-header_subtitle").addClass("hide");
+  $(".field-header_image").addClass("hide");
 
-    $(".field-header #id_header").change(function () {
-        var value = $(this).val();
+  $(".field-header #id_header").change(function () {
+    var value = $(this).val();
 
-        if (value == 'full') {
-            $(".field-header_title").show("slow").removeClass("hide");
-            $(".field-header_subtitle").show("slow").removeClass("hide");
-        } else {
-            $(".field-header_title").hide("slow").addClass("hide");
-            $(".field-header_subtitle").hide("slow").addClass("hide");
-        }
+    if (value == "full") {
+      $(".field-header_title").show("fast").removeClass("hide");
+      $(".field-header_subtitle").show("fast").removeClass("hide");
+    } else {
+      $(".field-header_title").hide("fast").addClass("hide");
+      $(".field-header_subtitle").hide("fast").addClass("hide");
+    }
 
-        if (value == 'image') {
-            $(".field-header_image").show("slow").removeClass("hide");
-        } else {
-            $(".field-header_image").hide("slow").addClass("hide");
-        }
-    }).change();
+    if (value == "image") {
+      $(".field-header_image").show("fast").removeClass("hide");
+    } else {
+      $(".field-header_image").hide("fast").addClass("hide");
+    }
+  }).change();
 });
 })(jQuery);
