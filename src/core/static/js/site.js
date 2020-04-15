99fa50c775fc64c8a54f98da6f325962139c4893
Guus Hoekman <mail@guushoekman.com>
2020-04-15 15:06:22 +0200

    markdown in forum with markdown help section

--- src/core/static/js/site.js
+++ src/core/static/js/site.js
@@ -4,3 +4,8 @@ function scrollToID(id){
    scrollTop: $("#" + id).offset().top
   }, "slow");
 }
+
+// open and close markdown help box -- here because it's not possible to add jquery to includes
+$(".open-markdown-help, .close-markdown-help").click(function() {
+  $(".markdown-help").toggle()
+})
