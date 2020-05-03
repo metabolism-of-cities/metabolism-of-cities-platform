14131f92dd165c45a3700eb517cafa126cdea5e9
Paul Hoekman <paul@penguinprotocols.com>
2020-05-03 20:53:00 +0200

    Project management improvements 1:00

--- src/core/views.py
+++ src/core/views.py
@@ -52,7 +52,7 @@ PAGE_ID = {
     "people": 12,
     "projects": 19,
     "library": 20,
-    "multimedia_library": 60,
+    "multimedia_library": 59,
     "multiplicity": 51,
     "platformu": 53,
     "stafcp": 55,
@@ -96,7 +96,7 @@ def getHeader(info):
 
     header_image = design.header_image.huge.url if design.header_image else None
     breadcrumbs = '<li class="breadcrumb-item"><a href="/">Home</a></li>'
-    if info.parent:
+    if info.parent and False:
         breadcrumbs += '<li class="breadcrumb-item"><a href="' + info.parent.get_absolute_url() + '">' + info.parent.name + '</a></li>'
     if design.header != "full":
         breadcrumbs += '<li class="breadcrumb-item active" aria-current="page">' + info.name + '</li>'
@@ -1055,7 +1055,7 @@ def forum_form(request, id=False):
 # MULTIMEDIA
 
 def multimedia(request):
-    webpage = get_object_or_404(Webpage, pk=PAGE_ID["multimedia_library"])
+    webpage = get_object_or_404(Project, pk=PAGE_ID["multimedia_library"])
     videos = Video.objects.all().order_by("-date_created")[:5]
     podcasts = LibraryItem.objects.filter(type__name="Podcast").order_by("-date_created")[:5]
     dataviz = LibraryItem.objects.filter(type__name="Image").order_by("-date_created")[:5]
@@ -1218,10 +1218,9 @@ def load_baseline(request):
         { "id": 43, "name": "Contribute", "parent": None, "slug": "/library/contribute/", "position": 5 },
         { "id": 46, "name": "Download", "parent": None, "slug": "/library/download/", "position": 3 },
 
-        { "id": 60, "name": "Multimedia Library", "parent": 19, "slug": "/multimedia/", "position": 1, "content": "<p>This library contains videos, podcasts, and data visualisations related to urban metabolism. Please find the latest items elow, or search for specific items in our search.</p>" },
-        { "id": 61, "name": "Videos", "parent": 60, "slug": "/multimedia/videos/", "position": 2, "content": "<p>This page contains videos related to urban metabolism.</p>", },
-        { "id": 62, "name": "Podcasts", "parent": 60, "slug": "/multimedia/podcasts/", "position": 3 , "content": "<p>On this page you will find podcasts related to urban metabolism.</p>"},
-        { "id": 63, "name": "Data Visualisations", "parent": 60, "slug": "/multimedia/datavisualizations/", "position": 4, "content": "<p>In October-December 2016, Metabolism of Cities ran a project around data visualisations. The goal was to explore ways in which information can be illustrated, take stock of work in this field, host online discussions and publish blog posts. Below you will see the list of the around <em>100 data visualisations</em> that were collected.</p><p><strong>Why visualise urban metabolism data</strong></p><p>When we think about urban metabolism and other urban environmental assessments, we often think about numbers, data analysis, formulas and tables. While we may be very familiar with our own case study, it is often very difficult to share the relevance of our results with other researchers or with the general public and to synthesise all this amount of knowledge into something easy to grasp. This is one of the main reasons why researchers use visualisation techniques. Visualising data can not only enable to summarise big amounts of numbers, but it can also make it easier to share them and use them as policy instruments.</p><p><strong>Add more data visualisations</strong></p><p>Many more data visualisations exist and Metabolism of Cities wants to make them accessible in one central space. You can help by submitting more visualisations through the <a href='../../about/task-forces/resources'>Resources Task Force</a>!</p>" },
+        { "id": 61, "name": "Videos", "parent": None, "slug": "/multimedia/videos/", "position": 2, "content": "<p>This page contains videos related to urban metabolism.</p>", },
+        { "id": 62, "name": "Podcasts", "parent": None, "slug": "/multimedia/podcasts/", "position": 3 , "content": "<p>On this page you will find podcasts related to urban metabolism.</p>"},
+        { "id": 63, "name": "Data Visualisations", "parent": None, "slug": "/multimedia/datavisualizations/", "position": 4, "content": "<p>In October-December 2016, Metabolism of Cities ran a project around data visualisations. The goal was to explore ways in which information can be illustrated, take stock of work in this field, host online discussions and publish blog posts. Below you will see the list of the around <em>100 data visualisations</em> that were collected.</p><p><strong>Why visualise urban metabolism data</strong></p><p>When we think about urban metabolism and other urban environmental assessments, we often think about numbers, data analysis, formulas and tables. While we may be very familiar with our own case study, it is often very difficult to share the relevance of our results with other researchers or with the general public and to synthesise all this amount of knowledge into something easy to grasp. This is one of the main reasons why researchers use visualisation techniques. Visualising data can not only enable to summarise big amounts of numbers, but it can also make it easier to share them and use them as policy instruments.</p><p><strong>Add more data visualisations</strong></p><p>Many more data visualisations exist and Metabolism of Cities wants to make them accessible in one central space. You can help by submitting more visualisations through the <a href='../../about/task-forces/resources'>Resources Task Force</a>!</p>" },
 
 
         { "id": 48, "name": "By method", "parent": 40, "slug": "/library/casestudies/methods/", "position": 2 },
@@ -1277,7 +1276,8 @@ def load_baseline(request):
             is_internal = True,
             start_date = start,
         )
-    Webpage.objects.filter(id__in=[40,41,42,43,46]).update(parent_id=20)
+    Webpage.objects.filter(id__in=[40,41,42,43,46]).update(parent_id=20) # Library
+    Webpage.objects.filter(id__in=[61,62,63]).update(parent_id=59) # Multimedia library
 
     messages.success(request, "UM, Community, Project, About pages were inserted/reset")
 
@@ -1460,7 +1460,7 @@ background-size: cover;
 /* background by SVGBackgrounds.com */
 }"""
         },
-        { "header": "small", "article": 60, "logo": "/logos/media-logo-multimedia.png", "css": """.top-layer {
+        { "header": "small", "article": 59, "logo": "/logos/media-logo-multimedia.png", "css": """.top-layer {
 background-color: #271a00;
 background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpath fill='%23251900' d='M486 705.8c-109.3-21.8-223.4-32.2-335.3-19.4C99.5 692.1 49 703 0 719.8V800h843.8c-115.9-33.2-230.8-68.1-347.6-92.2C492.8 707.1 489.4 706.5 486 705.8z'/%3E%3Cpath fill='%23231900' d='M1600 0H0v719.8c49-16.8 99.5-27.8 150.7-33.5c111.9-12.7 226-2.4 335.3 19.4c3.4 0.7 6.8 1.4 10.2 2c116.8 24 231.7 59 347.6 92.2H1600V0z'/%3E%3Cpath fill='%23211800' d='M478.4 581c3.2 0.8 6.4 1.7 9.5 2.5c196.2 52.5 388.7 133.5 593.5 176.6c174.2 36.6 349.5 29.2 518.6-10.2V0H0v574.9c52.3-17.6 106.5-27.7 161.1-30.9C268.4 537.4 375.7 554.2 478.4 581z'/%3E%3Cpath fill='%231f1800' d='M0 0v429.4c55.6-18.4 113.5-27.3 171.4-27.7c102.8-0.8 203.2 22.7 299.3 54.5c3 1 5.9 2 8.9 3c183.6 62 365.7 146.1 562.4 192.1c186.7 43.7 376.3 34.4 557.9-12.6V0H0z'/%3E%3Cpath fill='%231d1700' d='M181.8 259.4c98.2 6 191.9 35.2 281.3 72.1c2.8 1.1 5.5 2.3 8.3 3.4c171 71.6 342.7 158.5 531.3 207.7c198.8 51.8 403.4 40.8 597.3-14.8V0H0v283.2C59 263.6 120.6 255.7 181.8 259.4z'/%3E%3Cpath fill='%23231f08' d='M1600 0H0v136.3c62.3-20.9 127.7-27.5 192.2-19.2c93.6 12.1 180.5 47.7 263.3 89.6c2.6 1.3 5.1 2.6 7.7 3.9c158.4 81.1 319.7 170.9 500.3 223.2c210.5 61 430.8 49 636.6-16.6V0z'/%3E%3Cpath fill='%232a270e' d='M454.9 86.3C600.7 177 751.6 269.3 924.1 325c208.6 67.4 431.3 60.8 637.9-5.3c12.8-4.1 25.4-8.4 38.1-12.9V0H288.1c56 21.3 108.7 50.6 159.7 82C450.2 83.4 452.5 84.9 454.9 86.3z'/%3E%3Cpath fill='%23312f12' d='M1600 0H498c118.1 85.8 243.5 164.5 386.8 216.2c191.8 69.2 400 74.7 595 21.1c40.8-11.2 81.1-25.2 120.3-41.7V0z'/%3E%3Cpath fill='%23383716' d='M1397.5 154.8c47.2-10.6 93.6-25.3 138.6-43.8c21.7-8.9 43-18.8 63.9-29.5V0H643.4c62.9 41.7 129.7 78.2 202.1 107.4C1020.4 178.1 1214.2 196.1 1397.5 154.8z'/%3E%3Cpath fill='%2340401a' d='M1315.3 72.4c75.3-12.6 148.9-37.1 216.8-72.4h-723C966.8 71 1144.7 101 1315.3 72.4z'/%3E%3C/g%3E%3C/svg%3E");
 background-attachment: fixed;
@@ -1817,6 +1817,7 @@ def dataimport(request):
                     info.research_interests = row["research_interests"]
                     info.status = row["status"]
                     info.content = row["profile"]
+                    info.image = row["image"] if row["image"] else None
                     info.save()
                     if row["site_id"]:
                         info.site.add(row["site_id"])
