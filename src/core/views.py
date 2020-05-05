7c4e447958102663deb14b51e87af6538edba452
​christian <christian@penguinprotocols.com>
2020-05-05 14:47:59 +0200

    fixed json format

--- src/core/views.py
+++ src/core/views.py
@@ -623,13 +623,15 @@ def stafcp_upload_gis_file(request, id=None):
     return render(request, "stafcp/upload/gis.file.html", load_specific_design(context, PAGE_ID["stafcp"]))
 
 def stafcp_upload_gis_verify(request, id):
+    import json
     from django.contrib.gis.gdal import DataSource
     file = settings.MEDIA_ROOT + "/shapefiles/District_Municipalities_2016.shp"
     ds = DataSource(file)
     layer = ds[0]
-    geojson = []
+    geojson = {}
     for details in layer:
-        geojson.append(details.geom.json)
+        geojson = details.geom.json
+    geojson = json.loads(geojson)
     context = {
         "design_link": "/admin/core/articledesign/" + str(PAGE_ID["stafcp"]) + "/change/",
         "geojson": geojson,
