46771bfbee41cdbce3f73a7bbfcef7734e30aa87
Paul Hoekman <paul@penguinprotocols.com>
2020-05-08 14:28:25 +0200

    Change admin 3

--- src/core/admin.py
+++ src/core/admin.py
@@ -167,7 +167,7 @@ class LogEntryAdmin(admin.ModelAdmin):
 
 class UserAdmin(admin.ModelAdmin):
      list_display = ["username", "email", "is_staff", "is_active"]
-     list_filter = ["username", "email", "is_staff", "is_active"]
+     list_filter = ["is_staff", "is_active"]
      search_fields = ["username", "email"]
 
 admin_site.register(Tag, TagAdmin)
