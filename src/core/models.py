7032e172af39880c4f72d83021bcf4a1dd8bf9ef
Paul Hoekman <paul@penguinprotocols.com>
2020-05-13 21:29:18 +0200

    Create badges 20

--- src/core/models.py
+++ src/core/models.py
@@ -662,22 +662,25 @@ class WorkLog(models.Model):
     type = models.CharField(max_length=40, choices=TYPE)
     activity = models.ForeignKey(WorkActivity, on_delete=models.CASCADE, null=True, blank=True)
     related_to = models.ForeignKey(Record, on_delete=models.CASCADE, null=True, blank=True, related_name="workpieces_list")
-    assigned_to = models.ManyToManyField(People)
     tags = models.ManyToManyField(Tag, blank=True)
+    #assigned_to = models.ForeignKey(People, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-#class Badge(models.Model):
-#    name = models.CharField(max_length=255)
-#    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True, limit_choices_to={"is_internal": True})
-#    TYPE = [
-#        ("bronze", "Bronze"),
-#        ("silver", "Silver"),
-#        ("gold", "Gold"),
-#    ]
-#    type = models.CharField(max_length=20, choices=TYPE)
-#    description = models.CharField(max_length=255)
-    
-#    def __str__(self):
-#        return self.name
+class Badge(models.Model):
+
+    class BadgeType(models.IntegerChoices):
+        BRONZE = 1, "Bronze"
+        SILVER = 2, "Silver"
+        GOLD = 3, "Gold"
+
+    type = models.IntegerField(choices=BadgeType.choices, db_index=True)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True, limit_choices_to={"is_internal": True})
+    worktype = models.ForeignKey(WorkActivity, on_delete=models.CASCADE, null=True, blank=True)
+    required_quantity = models.PositiveSmallIntegerField(null=True, blank=True)
+   
+    def __str__(self):
+        return self.name
