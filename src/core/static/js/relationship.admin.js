c51b0e94c00bf82b24e7836e06e9a497cfc4dd24
​christian <christian@penguinprotocols.com>
2020-05-19 14:17:59 +0200

    displayed custom message

---
+++ src/core/static/js/relationship.admin.js
@@ -0,0 +1,5 @@
+(function ($) {
+  $(document).ready(function(){
+    $("#changelist").prepend("<div class=\"warning-danger\">Please don't edit these information - only the programming team should make changes here.</div>");
+  });
+})(jQuery);
