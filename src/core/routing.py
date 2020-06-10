695bfffc74d0c21910710eadbd7059d78b31e144
Fernando Larios <fernando@penguinprotocols.com>
2020-06-10 01:13:22 -0600

    connection the routing files and activating the websockets

--- src/core/routing.py
+++ src/core/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import core.chat_routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            core.chat_routing.websocket_urlpatterns
+        )
+    ),
 })
