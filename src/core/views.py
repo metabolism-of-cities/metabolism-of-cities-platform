5e790f4291e5b82087ac577952c958938c88a1b8
Paul Hoekman <paul@penguinprotocols.com>
2020-06-23 21:09:10 +0200

    Homepage management 30

--- src/core/views.py
+++ src/core/views.py
@@ -338,9 +338,9 @@ def user_profile(request, project=None):
 def index(request):
     context = {
         "header_title": "Metabolism of Cities",
-        "header_subtitle": "Your hub for anyting around urban metabolism",
+        "header_subtitle": "Your hub for everything around urban metabolism",
         "show_project_design": True,
-        "projects": Project.objects.filter(pk__in=[2,3,4,8,16,3458]),
+        "projects": Project.objects.filter(pk__in=[2,3,4,8,16,18]),
     }
     return render(request, "index.html", context)
 
