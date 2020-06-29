96aefef05e934fc27441823b4804a86a64b8dca2
Guus Hoekman <mail@guushoekman.com>
2020-06-29 17:19:47 +0200

    form fixes

--- src/core/static/js/site.js
+++ src/core/static/js/site.js
@@ -9,3 +9,20 @@ function scrollToID(id){
 $(".open-markdown-help, .close-markdown-help").click(function() {
   $(".markdown-help").toggle()
 })
+
+// form check for urls
+$("input[type='url']").change(function() {
+  let input = $(this)
+  let value = input.val();
+
+  value.trim();
+
+  if (!(value.match("^http"))) {
+    value = "http://" + value;
+  }
+
+  input.val(value);
+})
+
+// form fix for date picker on mac
+$("input[type='date']").attr("placeholder", "Format as YYYY-MM-DD");
