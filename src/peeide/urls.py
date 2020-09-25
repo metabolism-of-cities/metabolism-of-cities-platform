93ad36ca3e2c0c8eb223a640a957b670ee9e6e0b
Paul Hoekman <paul@penguinprotocols.com>
2020-09-25 19:52:49 +0200

    PEEIDE config 15

--- src/peeide/urls.py
+++ src/peeide/urls.py
@@ -7,6 +7,6 @@ from ie.urls_library_baseline import baseline_library_urlpatterns
 
 app_name = "peeide"
 urlpatterns = baseline_urlpatterns + baseline_library_urlpatterns + [
-    path("", views.index, name="index"),
+    path("", core.article, {"id": 51471}, name="index"),
     path("<slug:slug>/", core.article, name="page"),
 ]
