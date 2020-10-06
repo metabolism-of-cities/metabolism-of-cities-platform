cec906d65b7fb6eb9f589bef21b719a372714292
Guus Hoekman <mail@guushoekman.com>
2020-10-06 11:52:57 +0200

    choropleth folium example

--- src/stocks/views.py
+++ src/stocks/views.py
@@ -112,6 +112,34 @@ def map(request, slug, id):
 
     return render(request, "stocks/map.html", context)
 
+def choropleth(request):
+
+    state_geo = "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/us-states.json"
+    state_unemployment = "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/US_Unemployment_Oct2012.csv"
+    state_data = pd.read_csv(state_unemployment)
+
+    map = folium.Map(location=[48, -102], zoom_start=3)
+
+    folium.Choropleth(
+        geo_data=state_geo,
+        name='choropleth',
+        data=state_data,
+        columns=['State', 'Unemployment'],
+        key_on='feature.id',
+        fill_color='YlGn',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name='Unemployment Rate (%)'
+    ).add_to(map)
+
+    folium.LayerControl().add_to(map)
+
+    context = {
+        "map": map._repr_html_() if map else None,
+    }
+
+    return render(request, "stocks/map.html", context)
+
 def compare(request, slug):
     context = {
         "compare": True,
