83e544b248909b34a281399b2f3d0bd742124a50
Guus Hoekman <mail@guushoekman.com>
2020-12-06 13:43:46 +0100

    evaluation plans cityloops

--- src/cityloops/views.py
+++ src/cityloops/views.py
@@ -143,9 +143,19 @@ def eurostat_grid(request):
     return render(request, "cityloops/eurostat.grid.html", context)
 
 def circular_city(request):
-    info = get_object_or_404(Project, pk=request.project)
-    webpage = get_object_or_404(Webpage, pk=50439)
     context = {
         "title": "Circular City",
     }
     return render(request, "cityloops/circular-city.html", context)
+
+def evaluation_plans(request):
+    context = {
+        "title": "Evaluation plans",
+    }
+    return render(request, "cityloops/evaluation-plans.html", context)
+
+def evaluation_plans_create(request):
+    context = {
+        "title": "Create evaluation plan",
+    }
+    return render(request, "cityloops/evaluation-plans.create.html", context)
