a5e921c8ea46394d234c7fbead063d7f8930de81
Guus Hoekman <mail@guushoekman.com>
2021-05-14 12:08:16 +0200

    ndee controlpanel

--- src/peeide/views.py
+++ src/peeide/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q, Count
 from django.http import Http404, HttpResponseRedirect, JsonResponse, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 
 from django.utils import timezone
 import pytz
@@ -90,3 +91,107 @@ def library_list(request, id=None):
 
     return render(request, "peeide/library.list.html", context)
 
+def news_list(request, header_subtitle=None):
+    project = get_object_or_404(Project, pk=request.project)
+    list = News.objects.filter(projects=project).distinct()
+
+    context = {
+        "list": list[3:],
+        "shortlist": list[:3],
+        "add_link": "/admin/core/news/add/",
+        "header_title": "News",
+        "header_subtitle": header_subtitle,
+        "menu": "news",
+    }
+    return render(request, "peeide/news.list.html", context)
+
+@login_required
+def controlpanel_project_form(request, slug=None, id=None):
+
+    curator = False
+    if has_permission(request, request.project, ["curator"]):
+        curator = True
+
+    project = get_object_or_404(Project, pk=request.project)
+
+    ModelForm = modelform_factory(
+        PublicProject,
+        fields=["name", "url", "start_date", "end_date", "image", "part_of_project"],
+        labels={"image": "Image", "url": "Website URL", "part_of_project": "Project"},
+        )
+    if id:
+        info = get_object_or_404(PublicProject, pk=id)
+        form = ModelForm(request.POST or None, request.FILES or None, instance=info)
+    else:
+        form = ModelForm(request.POST or None, request.FILES or None, initial={"part_of_project": request.project})
+        info = None
+
+    if request.method == "POST":
+        if "delete" in request.POST:
+            info.is_deleted = True
+            info.save()
+            messages.success(request, "The project was deleted.")
+            return redirect(request.GET.get("next"))
+        elif form.is_valid():
+            info = form.save(commit=False)
+            info.description = request.POST.get("description")
+
+            if not info.meta_data:
+                info.meta_data = {}
+
+            info.meta_data["project_leader"] = request.POST.get("project_leader")
+            info.meta_data["research_team"] = request.POST.get("research_team")
+            info.meta_data["researcher"] = request.POST.get("researcher")
+            info.meta_data["institution"] = request.POST.get("institution")
+            info.save()
+
+            messages.success(request, "The information was saved.")
+
+            if not id:
+                RecordRelationship.objects.create(
+                    record_parent = request.user.people,
+                    record_child = info,
+                    relationship_id = RELATIONSHIP_ID["uploader"],
+                )
+
+                work = Work.objects.create(
+                    status = Work.WorkStatus.COMPLETED,
+                    part_of_project = project,
+                    workactivity_id = 31, # Need to add new activity and update this TODO!
+                    related_to = info,
+                    assigned_to = request.user.people,
+                    name = "Adding new project",
+                )
+                message = Message.objects.create(posted_by=request.user.people, parent=work, name="Status change", description="Task was completed")
+
+            if "next" in request.GET:
+                return redirect(request.GET.get("next"))
+            else:
+                return redirect(project.slug + ":controlpanel_projects")
+        else:
+            messages.error(request, "We could not save your form, please fill out all fields")
+
+    context = {
+        "form": form,
+        "title": "Add project" if not id else "Edit project",
+        "load_markdown": True,
+        "curator": curator,
+        "info": info,
+    }
+
+    return render(request, "controlpanel/project.form.html", context)
+
+@login_required
+def controlpanel_projects(request, type=None):
+    if not has_permission(request, request.project, ["curator", "admin", "publisher"]):
+        unauthorized_access(request)
+
+    project = request.project
+    list = PublicProject.objects.filter(part_of_project_id=project)
+
+    context = {
+        "load_datatables": True,
+        "list": list,
+        "type": type,
+    }
+    return render(request, "controlpanel/projects.html", context)
