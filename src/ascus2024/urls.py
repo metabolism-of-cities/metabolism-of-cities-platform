2ec0db2fcb77dee77fd4207c110dd084608389a4
Paul Hoekman <p@ulhoekman.com>
2023-10-21 10:57:28 +0200

    Configure subtitle ascus

--- src/ascus2024/urls.py
+++ src/ascus2024/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import urls
 from ie.urls_baseline import baseline_urlpatterns
 from . import views
 from community import views as community
+from core import views as core
 
 app_name = "ascus2024"
 
@@ -10,4 +11,5 @@ urlpatterns = baseline_urlpatterns + [
     path("", views.index, name="index"),
     path("event-calendar/", community.event_list, name="events"),
     path("event-calendar/<int:id>/<slug:slug>/", community.event, name="event"),
+    path("<slug:slug>/", core.article, { "subtitle": "Actionable Science for Urban Sustainability · 20-22 March 2024", "project": 1018839}, name="article"),
 ]
